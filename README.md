cbd13198ba3043c5ce4eccbd54fdda8b956afb26
cesar-gz <cesarg7@csu.fullerton.edu>
2023-06-23 21:33:57 -0700

    practicing with AI

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a AI project I am developing that will serve four functions for me
--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import openai
 import os
 
+from config import DTD
+
 # user, system, assistant
 
 prompt = """
-    You are a vacation planner. The user will provide locations and prefered
-    climates. Then you will give them great vacation spots/activities.
+    You are a master software developer. The user will provide questions about how
+    to code, program, or create anything. The user will also provide generic questions
+    that you are allowed to use the Internet to answer with only accurate and factual answers.
+    Then you will give them as correct answers that you do not make up. If you can not find
+    a correct answer on the Internet or to your knowledge, respond with a funny comment that
+    the comedian Bill Burr would say.
 
-    If the user doesn't mention locations/vactions/climates or anything similar please
-    respond with "I am a vacation planner. Ask a different robot, nerd."
+    When the user starts the program, write a funny joke or random fact from the Internet.
 """
 
 messages = [{"role": "system", "content": prompt}]
