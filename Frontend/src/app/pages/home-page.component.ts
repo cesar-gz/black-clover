305a25d48f03ad0b35f22c0458fa795f8c7eeb9f
cesar-gz <cesarg7@csu.fullerton.edu>
2023-07-12 22:30:59 -0700

    added hover text, styling, and footer

--- Frontend/src/app/pages/home-page.component.ts
+++ Frontend/src/app/pages/home-page.component.ts
@@ -17,14 +17,29 @@ import { Router } from '@angular/router';
         </div>
       </div>
 
+        <p class="hidden-text6">View the About page</p>
+
       <div class="relative-container">
+
         <img src="../assets/images/wizardking.png" alt="King" id="leaf1" (click)="handleLeafClick(1)">
+          <p class="hidden-text1">Subscribe to a newsletter service</p>
+
         <img src="../assets/images/yami.png" alt="Yami" id="leaf2" (click)="handleLeafClick(2)">
+          <p class="hidden-text2">Format your Emails</p>
+
         <img src="../assets/images/yuno.png" alt="Yuno" id="leaf3" (click)="handleLeafClick(3)">
+          <p class="hidden-text3">(To be determined later).</p>
+
         <img src="../assets/images/liebe.png" alt="Liebe" id="leaf4" (click)="handleLeafClick(4)">
+          <p class="hidden-text4">View the Market</p>
+
         <img src="../assets/images/asta.png" alt="Asta" id="leaf5" (click)="handleLeafClick(5)">
+          <p class="hidden-text5">Youtube Scrape Music</p>
+
       </div>
     </div>
+
+
   `,
   styleUrls: ['./home-page.component.css']
 })
